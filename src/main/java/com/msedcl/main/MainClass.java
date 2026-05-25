51a1bc3e568bfbaa14a99cc1a01d3e6b326d3caf
student16 <student16@PC16>
2026-05-25 14:04:22 +0530

    Merge

--- src/main/java/com/msedcl/main/MainClass.java
+++ src/main/java/com/msedcl/main/MainClass.java
@@ -1,7 +1,11 @@
 package com.msedcl.main;
 
+import com.msedcl.main.util.CreateEmployee;
+
 public class MainClass {
 public static void main(String[] args) {
 	System.out.println("new project begins here for employee - CRUD operations");
+	CreateEmployee createEmployee = new CreateEmployee();
+	createEmployee.createNewEmployee();
 }
 }
